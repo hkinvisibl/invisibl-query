dda3cf105e5465229a6849b608dd3dc9c3db8a9e
hkinvisibl <hardik.kesari@invisibl.io>
2025-12-23 10:39:54 +0000

    refactor: rename variables, restructure response

--- src/invisibl_query/client.py
+++ src/invisibl_query/client.py
@@ -3,19 +3,30 @@ from .utils import extract_metadata
 
 class QueryClient:
     def __init__(self):
-        self.api_url = ""
+        self.api_url = "http://localhost:3000/datascience/api/v1/projects/tre/cohorts/query"
 
-    def execute(self, sql: str):
-        # Get AWS identity and potentially table metadata
-        payload = extract_metadata(sql)
-        
+    def execute(self, query: str):
+        # Get AWS identity and table metadata
+        payload = extract_metadata(query)
+
+        print("payload - ", payload)
+        # payload["arn"] = 'arn:aws:sts::554248189203:assumed-role/quark-dev-wt-omop-analysis-sttgh-role/i-083b0b912b506ee78'
+       
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "gravity-userid":"hardik.kesari@invisibl.io",
+            "gravity-userinfo-token":"{\"tokenSecretName\" :\"user-hardik-kesari-invisibl-io-dffccc766\"}"
+        }
+
         try:
             response = requests.post(
-                f"{self.api_url}/v1/execute", 
+                f"{self.api_url}", 
+                headers=headers,
                 json=payload,
                 timeout=(2,900)
             )
             response.raise_for_status()
-            return response
+            return response.json()
         except requests.exceptions.RequestException as e:
-            return {"error": f"Failed to execute cohort: {str(e)}"}
+            return {"error": str(e)}
--- src/invisibl_query/utils.py
+++ src/invisibl_query/utils.py
@@ -4,17 +4,17 @@ from typing import Dict, Any, List, cast
 
 sts = boto3.client('sts')
 
-def extract_metadata(sql: str) -> Dict[str, Any]:
+def extract_metadata(query: str) -> Dict[str, Any]:
     # Get AWS Identity
     identity = cast(Dict[str, Any], sts.get_caller_identity())
     
     # SQL Table name(s) extraction
-    parser = Parser(sql)
+    parser = Parser(query)
     tables = cast(List[str], parser.tables)
     
     return {
         "arn": str(identity.get('Arn', 'unknown')),
         "account": str(identity.get('Account', 'unknown')),
         "tables": tables,
-        "sql": sql
+        "query": query
     }
